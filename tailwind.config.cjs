e9b9ee7d00f2e8ac676fa12ccbf84bf332f51475
thiago <thiago@dreamcode.io>
2023-03-04 17:34:39 -0300

    add animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
         default: "#000000",
         600: "#120f13",
         700: "#0B090C",
+        A: "rgba(18, 15, 19, 0.5)",
       },
       neutral: {
         DEFAULT: "#FAFAFB",
@@ -24,6 +25,7 @@ module.exports = {
       },
       gray: {
         100: "#3C393F",
+        400: "#1c1b1e",
       },
       red: {
         DEFAULT: "#F0402C",
@@ -35,6 +37,7 @@ module.exports = {
         DEFAULT: "#2F80ED",
         50: "#2F80ED",
         400: "#2D9CDB",
+        600: "#065fa3",
       },
       indigo: {
         DEFAULT: "#282051",
@@ -44,6 +47,46 @@ module.exports = {
     extend: {
       fontFamily: {
         NotoSans: ["Noto Sans", "sans-serif"],
+      },
+      willChange: {
+        "transform-opacity": "transform, opacity",
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: "translateY(-2px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
+        slideLeftAndFade: {
+          from: { opacity: 0, transform: "translateX(2px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: "translateY(2px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
+        slideRightAndFade: {
+          from: { opacity: 0, transform: "translateX(2px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
+          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
