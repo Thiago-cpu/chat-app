b04d9c0031b43e9a25a43cd466dc13463cd5a887
thiago <thiago@dreamcode.io>
2023-03-16 21:14:50 -0300

    add vercel

--- src/env.mjs
+++ src/env.mjs
@@ -1,9 +1,5 @@
 import { z } from "zod";
 
-/**
- * Specify your server-side environment variables schema here. This way you can ensure the app isn't
- * built with invalid env vars.
- */
 const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
